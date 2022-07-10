4ac76b6cfab2198c3b311be608a556241648596d
Ghost MacBookPro <pedleo93@gmail.com>
2022-07-10 13:55:14 -0500

    Cambios

--- index.php
+++ index.php
@@ -12,5 +12,5 @@
   <app-root></app-root>
   <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.5/dist/umd/popper.min.js" integrity="sha384-Xe+8cL9oJa6tN/veChSP7q+mnSPaj5Bcu9mPX5F5xIGE0DVittaqT5lorf0EI7Vk" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.min.js" integrity="sha384-kjU+l4N0Yf4ZOJErLsIcvOU2qSb74wXpOhqTvwVx3OElZRweTnQ6d31fXEoRD1Jy" crossorigin="anonymous"></script>
-<script src="runtime-es2015.21298e5e706979b0668a.js" type="module"></script><script src="runtime-es5.21298e5e706979b0668a.js" nomodule defer></script><script src="polyfills-es5.177e85a9724683782539.js" nomodule defer></script><script src="polyfills-es2015.f332a089ad1600448873.js" type="module"></script><script src="main-es2015.cfdf8fdbe42a5572f08b.js" type="module"></script><script src="main-es5.cfdf8fdbe42a5572f08b.js" nomodule defer></script></body>
+<script src="runtime-es2015.21298e5e706979b0668a.js" type="module"></script><script src="runtime-es5.21298e5e706979b0668a.js" nomodule defer></script><script src="polyfills-es5.177e85a9724683782539.js" nomodule defer></script><script src="polyfills-es2015.f332a089ad1600448873.js" type="module"></script><script src="main-es2015.cd0ad63584e399ca527f.js" type="module"></script><script src="main-es5.cd0ad63584e399ca527f.js" nomodule defer></script></body>
 </html>
